7f09913fa354495cba5592784eabd3abb4b635e7
aanchal <7.akirashah@gmail.com>
2021-06-30 13:49:27 +0530

    creating a new todo through input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //CHECK OFF SPECIFIC TODOS BY CLICKING
-$("li").click(function(){
+$("ul").on('click', "li", function(){ // select parent element ul and on('click') on li for upcoming changes
 	$(this).toggleClass("completed"); //one line instead of below code
 	//to understand above concept
 	// //check if li is grey
@@ -17,11 +17,28 @@ $("li").click(function(){
 
 //CLICK ON X TO DELETE A TODO
 //event  bubbling occurs here i.e. events of parent elements are triggered span>li>ul>body>
-$("span").click(function(event){
+$("ul").on('click', "span", function(event){
 	$(this).parent().fadeOut(500, function(){ //selecting span element through first this, then selecting parent element li
 											  // giving a fadeOut effect which just fades out doesn't remove
 											  // so giving effects for 500ms and removing it using function
 		$(this).remove();					  // here second this is parent element li and removing li then
 	}); 
 	event.stopPropagation(); //stops event bubbling
+});
+
+
+//Before creating a new To-Do 
+//NOTE : the difference between click() and on('click') is that 
+// click() only adds listeners to existing element and
+// on('click') add listeners to all potential upcoming future elements
+
+//CREATING A TODO THROUGH INPUT
+$("input[type='text']").keypress(function(e){
+	if (e.which === 13){
+		//grabbing the new todo from input
+		var newTodo = $(this).val(); // get value
+		$(this).val(""); // set value to blank 
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + newTodo + "</li>"); // append new li to ul
+	}
 });
