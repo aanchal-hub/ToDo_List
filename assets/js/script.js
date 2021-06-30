610cdd6f2f75779b6fce2b0a4244fe178f090740
aanchal <7.akirashah@gmail.com>
2021-06-30 13:02:24 +0530

    Deleting feature added; Note: event bubbling is tricky stuff keep in mind for future use

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,12 @@ $("li").click(function(){
 });
 
 //CLICK ON X TO DELETE A TODO
-$("span").click(function(){
-	alert("done");
+//event  bubbling occurs here i.e. events of parent elements are triggered span>li>ul>body>
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){ //selecting span element through first this, then selecting parent element li
+											  // giving a fadeOut effect which just fades out doesn't remove
+											  // so giving effects for 500ms and removing it using function
+		$(this).remove();					  // here second this is parent element li and removing li then
+	}); 
+	event.stopPropagation(); //stops event bubbling
 });
